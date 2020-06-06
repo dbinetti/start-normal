f7cbb32c13435dcabbbb67ea77c11ac1fc564bed
David Binetti <dbinetti@gmail.com>
2020-06-06 10:57:51 -0700

    Letter and disclaimer

---
+++ project/app/messages.py
@@ -0,0 +1,34 @@
+# Local
+# First-Party
+from django_rq import job
+
+from .models import *
+from .tasks import build_email
+
+
+@job
+def send_email(email):
+    return email.send()
+
+
+def send_first():
+
+    signatures = Signature.objects.exclude(
+        email='',
+    )
+
+    for signature in signatures:
+
+        template = 'emails/first.txt'
+        context = {'signature': signature}
+        subject = "Start Normal - Update #1"
+        to = [signature.email]
+
+        email = build_email(
+            template=template,
+            context=context,
+            subject=subject,
+            to=to,
+        )
+        send_email.delay(email)
+        return
--- project/app/tasks.py
+++ project/app/tasks.py
@@ -1,15 +1,50 @@
+# Standard Libary
+from textwrap import wrap
+
+# Django
 from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
-# from django_rq import job
+# First-Party
+from django_rq import job
 
 
-# @job
-def queue_email(subject, body, to):
-    mail = EmailMessage(
+# Utility
+def build_email(template, context, subject, to, cc=[], bcc=[], attachments=[]):
+    # Clean as necessary
+    # Remove commas
+    # to = [x.replace(",", "") for x in to]
+    # cc = [x.replace(",", "") for x in cc]
+    # bcc = [x.replace(",", "") for x in bcc]
+
+    # # Remove duplicate emails, keeping only the first
+    # full = []
+    # clean_to = []
+    # clean_cc = []
+    # clean_bcc = []
+    # for address in to:
+    #     if not address.partition("<")[2].partition(">")[0] in full:
+    #         clean_to.append(address)
+    #     full.append(address.partition("<")[2].partition(">")[0])
+    # for address in cc:
+    #     if not address.partition("<")[2].partition(">")[0] in full:
+    #         clean_cc.append(address)
+    #     full.append(address.partition("<")[2].partition(">")[0])
+    # for address in bcc:
+    #     if not address.partition("<")[2].partition(">")[0] in full:
+    #         clean_bcc.append(address)
+    #     full.append(address.partition("<")[2].partition(">")[0])
+
+
+    body = render_to_string(template, context)
+    # wrapped = wrap(body)
+    email = EmailMessage(
         subject=subject,
         body=body,
         from_email='David Binetti <dbinetti@gmail.com>',
         to=to,
-        bcc=['dbinetti@gmail.com'],
     )
-    return mail.send()
+    for attachment in attachments:
+        with attachment[1].open() as f:
+            email.attach(attachment[0], f.read(), attachment[2])
+    return email
