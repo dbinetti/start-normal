91de5754d4925317d87323c0c3be2ceee6431a98
David Binetti <dbinetti@gmail.com>
2020-06-05 17:31:03 -0700

    simple tests

--- project/app/tests.py
+++ project/app/tests.py
@@ -1,2 +1,45 @@
-def test_simple():
-    assert True
+# Django
+from django.test import TestCase
+from django.urls import reverse
+
+# First-Party
+from app.forms import SignatureForm
+
+
+class IndexView(TestCase):
+    def test_index(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_transcript(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('transcript'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_letter(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('letter'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_thanks(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('thanks'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_signature_form(self):
+        form_data = {
+            'name': 'foobar',
+            'email': 'foo@bar.com',
+            'city': 'city',
+        }
+        form = SignatureForm(data=form_data)
+        self.assertTrue(form.is_valid())
