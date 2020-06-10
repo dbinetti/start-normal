f533c7ec4ad5e538530de416363057b6242b5b61
David Binetti <dbinetti@gmail.com>
2020-06-10 07:46:50 -0700

    dsafd

--- project/app/tests.py
+++ project/app/tests.py
@@ -14,12 +14,12 @@ class IndexView(TestCase):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
 
-    def test_transcript(self):
-        """
-        If no questions exist, an appropriate message is displayed.
-        """
-        response = self.client.get(reverse('transcript'))
-        self.assertEqual(response.status_code, 200)
+    # def test_transcript(self):
+    #     """
+    #     If no questions exist, an appropriate message is displayed.
+    #     """
+    #     response = self.client.get(reverse('transcript'))
+    #     self.assertEqual(response.status_code, 200)
 
     def test_letter(self):
         """
