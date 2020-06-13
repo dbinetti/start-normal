106d39cb9ef08a25f96497141aaf979c93a31472
David Binetti <dbinetti@gmail.com>
2020-06-13 13:39:00 -0700

    Move data to User model

--- project/app/admin.py
+++ project/app/admin.py
@@ -54,24 +54,75 @@ class SignatureAdmin(admin.ModelAdmin):
     ]
 
 
+
+
+
+
 class CustomUserAdmin(UserAdmin):
+    save_on_top = True
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ('email', 'is_staff', 'is_active',)
-    list_filter = ('email', 'is_staff', 'is_active',)
+    list_display = [
+        'email',
+        'name',
+        'handle',
+        'location',
+        'is_public',
+        'is_subscribed',
+    ]
+    list_editable = [
+        'name',
+        'handle',
+        'location',
+        'is_public',
+        'is_subscribed',
+    ]
+    list_filter = [
+        'location',
+        'is_public',
+        'is_approved',
+    ]
+    search_fields = [
+        'name',
+        'email',
+    ]
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
+        (None, {
+            'fields': [
+                'is_approved',
+                'name',
+                'handle',
+                'email',
+                'phone',
+                'is_volunteer',
+                'is_public',
+                'is_subscribed',
+                'location',
+                'notes',
+            ]
+        }
+        ),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': [
+                'email',
+                'name',
+                'location',
+                'password1',
+                'password2',
+                'is_staff',
+                'is_active',
+            ]
+        }
         ),
     )
-    search_fields = ('email',)
-    ordering = ('email',)
+    ordering = [
+        'timestamp',
+    ]
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- project/app/models.py
+++ project/app/models.py
@@ -12,6 +12,7 @@ from .managers import CustomUserManager
 
 class Signature(models.Model):
 
+
     class Location(models.TextChoices):
         ATHERTON = 'ATH', ('Atherton')
         BELMONT = 'BEL', ('Belmont')
@@ -103,17 +104,97 @@ class Signature(models.Model):
 class CustomUser(AbstractUser):
     id = ShortUUIDField(
         primary_key=True,
-        default=shortuuid.uuid()[:8],
     )
 
     username = None
+
     email = models.EmailField(
-        'email address',
+        blank=False,
         unique=True,
+        help_text="""Your email is private and not shared.  We need it to manage preferences and send updates.  If you do not wish updates, unclick the 'Send Updates' checkbox.""",
+    )
+
+    class Location(models.TextChoices):
+        ATHERTON = 'ATH', ('Atherton')
+        BELMONT = 'BEL', ('Belmont')
+        BRISBANE = 'BRB', ('Brisbane')
+        BURLINGAME = 'BUR', ('Burlingame')
+        COLMA = 'COL', ('Colma')
+        DC = 'DC', ('Daly City')
+        EPA = 'EPA', ('East Palo Alto')
+        FC = 'FC', ('Foster City')
+        HMB = 'HMB', ('Half Moon Bay')
+        HILLSBOROUGH = 'HIL', ('Hillsborough')
+        MP = 'MP', ('Menlo Park')
+        MILLBRAE = 'MIL', ('Millbrae')
+        PACIFICA = 'PAC', ('Pacifica')
+        PV = 'PV', ('Portola Valley')
+        RC = 'RC', ('Redwood City')
+        SB = 'SB', ('San Bruno')
+        SC = 'SC', ('San Carlos')
+        SM = 'SM', ('San Mateo')
+        SSF = 'SSF', ('South San Francisco')
+        WOODSIDE = 'WS', ('Woodside')
+        UN = 'UN', ('Unincorporated San Mateo County')
+        OUT = 'OUT', ('Outside of San Mateo County')
+
+    name = models.CharField(
+        max_length=255,
+        default='Concerned Parent',
+        blank=False,
+        help_text="""Be aware your real name makes a real a difference.  However, we recognize the need for privacy so use something like 'Concerned Parent' or 'Father of Two' if you feel you must.  Your signature will be part of the public record.""",
+    )
+    handle = models.CharField(
+        max_length=255,
+        null=False,
+        blank=True,
+        help_text="""Your public name is how it will appear on the website.  This could be your real name (which is the most powerful), or first name initial last name, or something like 'Father of Two'.  If blank, defaults to your real name.""",
+    )
+    is_approved = models.BooleanField(
+        default=False,
+    )
+    city = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    is_public = models.BooleanField(
+        default=False,
+    )
+    is_subscribed = models.BooleanField(
+        default=True,
+    )
+    location = models.CharField(
+        max_length=255,
+        choices=Location.choices,
+        null=True,
+        help_text="""This helps provide specificity to Mr. Callagy.""",
+    )
+    phone = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    is_volunteer = models.BooleanField(
+        default=False,
+    )
+    notes = models.TextField(
+        max_length=255,
+        null=True,
+        blank=True,
+        default=None,
+        help_text="""Feel free to include anything you think we should know.  For instance, many have used this box to indicate they are teachers who are supportive of starting normal -- and we love to hear that!""",
+    )
+
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
     )
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = [
+        'name',
+        'location',
+    ]
 
     objects = CustomUserManager()
 
