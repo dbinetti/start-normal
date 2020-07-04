04159ea519dc641d6509535a337fb176389d36a1
David Binetti <dbinetti@gmail.com>
2020-07-03 17:44:10 -0700

    Really disable signals

--- project/app/apps.py
+++ project/app/apps.py
@@ -5,7 +5,4 @@ from django.apps import AppConfig
 class AppConfig(AppConfig):
     name = 'app'
     def ready(self):
-        from app.signals import (
-            # user_post_save,
-            # user_post_delete,
-        )
+        pass
