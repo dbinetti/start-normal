e847fd2509d852b3872e4b58d48d9f58eb6c3ea6
David Binetti <dbinetti@gmail.com>
2020-07-06 10:53:15 -0700

    Update indexes.py

--- project/app/indexes.py
+++ project/app/indexes.py
@@ -11,6 +11,15 @@ class DistrictIndex(AlgoliaIndex):
     fields = [
         'name',
         'slug',
+        'county',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'phone',
+        'website',
+        'get_doc_display',
+        'nces_district_id',
     ]
     geo_field = 'location'
     settings = {
@@ -20,29 +29,48 @@ class DistrictIndex(AlgoliaIndex):
             'county',
             'city',
             'state',
-            # 'attributesForFaceting': [
-            #     'city',
-            #     'state',
-            #     'county',
-            # ],
+            'nces_district_id',
+        ],
+        'attributesForFaceting': [
+            'city',
+            'state',
+            'county',
         ],
     }
     should_index = 'is_active'
 
 
-# class SchoolIndex(AlgoliaIndex):
-#     fields = [
-#         'name',
-#         'slug',
-#     ]
-#     geo_field = 'location'
-#     settings = {
-#         'searchableAttributes': [
-#             'name',
-#             'slug',
-#             'county',
-#             'city',
-#             'state',
-#         ],
-#     }
-#     should_index = 'is_active'
+class SchoolIndex(AlgoliaIndex):
+    fields = [
+        'name',
+        'slug',
+        'county',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'phone',
+        'website',
+        'get_soc_display',
+        'nces_school_id',
+        'eil',
+        'grades',
+        'district',
+    ]
+    geo_field = 'location'
+    settings = {
+        'searchableAttributes': [
+            'name',
+            'slug',
+            'county',
+            'city',
+            'state',
+            'nces_school_id',
+        ],
+        'attributesForFaceting': [
+            'city',
+            'state',
+            'county',
+        ],
+    }
+    should_index = 'is_active'
