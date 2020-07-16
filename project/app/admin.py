6571051359e8c702686fcdede4b117192bcbc406
David Binetti <dbinetti@gmail.com>
2020-07-16 16:44:03 -0600

    refactor remaining

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,9 +1,9 @@
 # Django
+# Third-Party
+from mptt.admin import MPTTModelAdmin
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as UserAdminBase
-
-# First-Party
-from mptt.admin import MPTTModelAdmin
 
 # Local
 from .forms import UserChangeForm
--- project/app/views.py
+++ project/app/views.py
@@ -2,6 +2,16 @@
 # Standard Library
 import json
 
+# Third-Party
+import django_rq
+import requests
+import shortuuid
+from auth0.v3.authentication import Database
+from auth0.v3.authentication import Logout
+from auth0.v3.exceptions import Auth0Error
+from dal import autocomplete
+from django_rq import job
+
 from django import forms
 from django.conf import settings
 from django.contrib import messages
@@ -22,16 +32,6 @@ from django.shortcuts import redirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.urls import reverse_lazy
-
-# First-Party
-import django_rq
-import requests
-import shortuuid
-from auth0.v3.authentication import Database
-from auth0.v3.authentication import Logout
-from auth0.v3.exceptions import Auth0Error
-from dal import autocomplete
-from django_rq import job
 
 # Local
 from .forms import AccountForm
@@ -487,9 +487,9 @@ def logout(request):
 # @staff_member_required
 # def report(request):
 #     report = Affiliation.objects.order_by(
-#         'organization',
+#         'school',
 #     ).values(
-#         'organization',
+#         'school',
 #     ).annotate(
 #         c=Count('id'),
 #     )
