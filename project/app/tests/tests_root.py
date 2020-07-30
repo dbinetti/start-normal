6dd9b6f56f7f76858c1489751af6bae20fde1185
David Binetti <dbinetti@gmail.com>
2020-07-30 05:00:15 -0700

    turn off deploy

--- project/app/tests/tests_root.py
+++ project/app/tests/tests_root.py
@@ -1,14 +1,14 @@
 # Django
-# Third-Party
-import pytest
-
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+# First-Party
+import pytest
+
 
 def test_deploy():
-    assert True
+    assert False
 
 
 def test_index_anon(anon_client):
