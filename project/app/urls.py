9a2aa536dadc1a75af61eaffd365f72b06e2b4b8
David Binetti <dbinetti@gmail.com>
2020-07-30 06:53:05 -0700

    fix url PK references

--- project/app/urls.py
+++ project/app/urls.py
@@ -17,18 +17,18 @@ urlpatterns = [
     # Authentication
     path('login', views.login, name='login'),
     path('signup/<kind>', views.signup, name='signup'),
-    path('join/<id>', views.join, name='join'),
+    path('join/<homeroom_id>', views.join, name='join'),
     path('callback', views.callback, name='callback'),
     path('logout', views.logout, name='logout'),
 
     # Account
     path('dashboard', views.dashboard, name='dashboard',),
     path('teacher', views.teacher, name='teacher',),
-    path('homeroom/<id>', views.homeroom, name='homeroom',),
+    path('homeroom/<homeroom_id>', views.homeroom, name='homeroom',),
     path('homeroom/<homeroom_id>/add-student', views.add_student, name='add-student',),
     path('homeroom/<homeroom_id>/<student_id>', views.add_classmate, name='add-classmate',),
     path('joinhomeroom', views.join_homeroom, name='join-homeroom',),
-    path('student/<id>', views.student, name='student',),
+    path('student/<student_id>', views.student, name='student',),
     path('parent', views.parent, name='parent',),
     path('share', views.share, name='share',),
     path('delete', views.delete, name='delete',),
@@ -39,5 +39,5 @@ urlpatterns = [
     path('school/<slug>', views.school, name='school'),
     path('search', views.search, name='search'),
     path('create-student', views.create_student, name='create-student'),
-    path('delete-student/<id>', views.delete_student, name='delete-student'),
+    path('delete-student/<student_id>', views.delete_student, name='delete-student'),
 ]
